e03e97ade0e7c011ac248122814dd098819c8546
bart-met-de-luxe-achternaam <bart.muelders@student.hu.nl>
2016-03-21 14:39:13 +0100

    geliktere tijd

--- spiegel/Clock.cs
+++ spiegel/Clock.cs
@@ -12,7 +12,7 @@ namespace spiegel
 {
     class Clock : Widget
     {
-        public Clock(Grid UiRoot) : base(UiRoot, 300, 100, new Thickness(10,10,10,10), HorizontalAlignment.Right, VerticalAlignment.Top, TimeSpan.FromSeconds(1))
+        public Clock(Grid UiRoot) : base(UiRoot, 100, 65, new Thickness(10,10,10,10), HorizontalAlignment.Center, VerticalAlignment.Top, TimeSpan.FromSeconds(1))
         {
         }
 
@@ -22,17 +22,24 @@ namespace spiegel
 
             clearWidget();
 
-            Grid grid = new Grid();
-
             TextBlock clock = new TextBlock();
             //todo leading/padding 0's
-            clock.Text = dateTime.DayOfWeek + " " + dateTime.Day.ToString() + "/" + dateTime.Month.ToString() + "\n" + dateTime.Hour + " : " + dateTime.Minute + " : " + dateTime.Second ;
+            clock.Text = dateTime.Hour + ":" + dateTime.Minute ;
             clock.Foreground = new SolidColorBrush(Colors.White);
-            clock.TextAlignment = TextAlignment.Right;
+            clock.TextAlignment = TextAlignment.Center;
+            clock.FontSize = 40;
+            clock.VerticalAlignment = VerticalAlignment.Top;
 
-            grid.Children.Add(clock);
+            TextBlock date = new TextBlock();
+            date.Text = dateTime.DayOfWeek + " " + dateTime.Day.ToString() + "-" + dateTime.Month.ToString();
+            date.Foreground = new SolidColorBrush(Colors.White);
+            date.TextAlignment = TextAlignment.Center;
+            date.FontSize = 14;
+            date.VerticalAlignment = VerticalAlignment.Bottom;
 
-            addToWidget(grid);
+
+            addToWidget(clock);
+            addToWidget(date);
         }
     }
 }
